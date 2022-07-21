081ca2ce0ab782a1a310b3bf4162d409ca212d7b
Javier Zapata <javierzv777@gmail.com>
2022-07-21 08:13:57 -0500

    loading in game detail

--- client/src/index.js
+++ client/src/index.js
@@ -8,8 +8,8 @@ import store from './store/store'
 import {BrowserRouter} from 'react-router-dom'
 import axios from 'axios'
 
-axios.defaults.baseURL =  "http://localhost:3001" ;
-// axios.defaults.baseURL =   "" ;
+// axios.defaults.baseURL =  "http://localhost:3001" ;
+axios.defaults.baseURL =   "" ;
 
 ReactDOM.render(
   
